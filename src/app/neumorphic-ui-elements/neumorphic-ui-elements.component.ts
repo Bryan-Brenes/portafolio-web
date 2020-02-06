a7807fcd199d85ddadee5fcd69802aa14212ff48
Bryan Brenes <bryanbrenes1@gmail.com>
2020-02-06 14:31:56 -0600

    arregle slider neumorphic ui elements

--- src/app/neumorphic-ui-elements/neumorphic-ui-elements.component.ts
+++ src/app/neumorphic-ui-elements/neumorphic-ui-elements.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NeumorphicUiElementsComponent implements OnInit {
 
+  private slider: HTMLInputElement;
+
   constructor() { }
 
   ngOnInit() {
+    this.slider = <HTMLInputElement>document.getElementById('slider');
+    this.slider.addEventListener('input', () => {
+      this.actualizarSlider();
+    })
+
+  }
+
+  actualizarSlider() {
+    let sliderIndicator = document.getElementById('slider-indicator')
+    let value: string = this.slider.value;
+    this.slider.style.background = `linear-gradient(90deg, #1758f1 0%, #37dbe0 ${value}%, #343a40 ${value}%)`
+    sliderIndicator.style.left = `${Number(value)}%`;
+    document.querySelector('#slider-indicator span').innerHTML = `${Number(value)}%`;
   }
 
 }
